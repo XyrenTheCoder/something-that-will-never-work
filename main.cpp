f330eb6ea1cec76b8daacb43a649c001abcd1d06
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-08-26 16:11:51 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,13 +12,13 @@ int main()
     cout << "Available commands: add (a), subtract (s), multiply (m), divide (d), sine (sin), cosine (cos), tangent (tan): \n";
     cin >> ans;
     if(ans == "a" || ans == "s" || ans == "m" || ans == "d" || ans == "sin" || ans == "cos" || ans == "tan"){
-        //python pass moment
+        continue; //python pass moment, bloated and ik
     }else{
         cout << "That option doesnt exist. Are u stupid?";
         return 69420;
     }
     
-    if(ans == "sin" || ans == "cos" || ans == "tan"){
+    if(ans == "sin" || ans == "cos" || ans == "tan" || ans == "sinh" || ans == "cosh" || ans == "tanh"){
         cout << "x = ?: ";
         cin >> num1;
     }else if(ans == "a" || ans == "s" || ans == "m" || ans == "d"){
@@ -36,12 +36,18 @@ int main()
         cout << num1 * num2;
     }else if(ans == "d"){
         cout << num1 / num2;
-    }else if(ans == "sin"){
+    }else if(ans == "sin"){ //trigo starts here
         cout << sin(num1);
     }else if(ans == "cos"){
         cout << cos(num1);
     }else if(ans == "tan"){
         cout << tan(num1);
+    }else if(ans == "sinh"){
+        cout << sinh(num1);
+    }else if(ans == "cosh"){
+        cout << cosh(num1);
+    }else if(ans == "tanh"){
+        cout << tanh(num1);
     };
     return 0;
 }
