c58b31c9d36e2d9e1771e78490bc16c8b9444869
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-08-26 20:51:23 +0800

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -4,11 +4,11 @@
 #include <complex>
 #include <cmath>
 
-using namespace std;
+using namespace std::complex_literals;
 
 int main()
 {
-    double x, y;
+    auto x, y;
     string ans;
     
     cout << "Available commands:\nBasic operations:\nadd (a), subtract (s), multiply (m), divide (d)\nTrigonometry:\nsine x (sin), cosine x (cos), tangent x (tan)\nInverse: arc sine (arcsin), arc cosine (arccos), arc tangent (arctan)\nHyperbolic:\nhyperbolic sine x (sinh), hyperbolic cosine x (cosh), hyperbolic tangent x (tanh)" << endl;
@@ -32,7 +32,7 @@ int main()
     bool singlchk = find(onearg, onearg + a, ans) != onearg + a; //checks if the math function need 1 or 2 args
     
     if(singlchk){
-        cout << "x = ?: ";
+        cout << "x = ? (in form of a + bi): ";
         cin >> x;
     }else{
         cout << "x = ?: ";
