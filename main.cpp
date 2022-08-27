d00fa2cac598ecb1655f31752b84783966d843b8
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-08-27 16:37:53 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@ int main()
     complex<double> x, y, o;
     string ans;
     
-    cout << "Available commands:\nBasic operations:\nadd (a), subtract (s), multiply (m), divide (d), absolute (abs)\nLogarithms:\ncommon base10 logarithm (log10), natural logarithm (ln)\nTrigonometry:\nsine x (sin), cosine x (cos), tangent x (tan)\nInverse:\narc sine (arcsin), arc cosine (arccos), arc tangent (arctan)\nHyperbolic:\nhyperbolic sine x (sinh), hyperbolic cosine x (cosh), hyperbolic tangent x (tanh)" << endl;
+    cout << "Available commands:\nBasic operations:\nadd (a), subtract (s), multiply (m), divide (d), absolute (abs), square root (sqrt)\nLogarithms:\ncommon base10 logarithm (log10), natural logarithm (ln)\nTrigonometry:\nsine x (sin), cosine x (cos), tangent x (tan)\nInverse:\narc sine (arcsin), arc cosine (arccos), arc tangent (arctan)\nHyperbolic:\nhyperbolic sine x (sinh), hyperbolic cosine x (cosh), hyperbolic tangent x (tanh)" << endl;
     cin >> ans;
     
-    string arr[] = {"a", "s", "m", "d", "abs", "log10", "ln", "sin", "cos", "tan", "arcsin", "arccos", "arctan", "sinh", "cosh", "tanh"}; //cmd list, just yeet every command name here
+    string arr[] = {"a", "s", "m", "d", "abs", "sqrt", "log10", "ln", "sin", "cos", "tan", "arcsin", "arccos", "arctan", "sinh", "cosh", "tanh"}; //cmd list, just yeet every command name here
     int n = sizeof(arr) / sizeof(*arr);
  
     bool e = find(arr, arr + n, ans) != arr + n;
@@ -25,7 +25,7 @@ int main()
         return 404; //404 not found but idk
     }
     
-    string onearg[] = {"abs", "log10", "ln", "sin", "cos", "tan", "arcsin", "arccos", "arctan", "sinh", "cosh", "tanh"};
+    string onearg[] = {"abs", "sqrt", "log10", "ln", "sin", "cos", "tan", "arcsin", "arccos", "arctan", "sinh", "cosh", "tanh"};
     int a = sizeof(onearg) / sizeof(*onearg);
     
     bool singlchk = find(onearg, onearg + a, ans) != onearg + a; //checks if the math function need 1 or 2 args
@@ -34,9 +34,9 @@ int main()
         cout << "x = ? (in form of a + bi, enter value as \"a, b\"): ";
         cin >> x;
     }else{
-        cout << "x = ? (in form of real numbers, enter value as \"x\"): ";
+        cout << "x = ? (in form of a + bi, enter value as \"a, b\"): ";
         cin >> x;
-        cout << "y = ? (in form of real numbers, enter value as \"y\"): ";
+        cout << "y = ? (in form of a + bi, enter value as \"a, b\"): ";
         cin >> y;
     }
         
