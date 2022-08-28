5342b3de51678b79c416e88259d485487ac46039
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-08-28 17:43:35 +0800

    change complex var "x" to "z", add real 1 arg check

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@
 using namespace std;
 
 int main(){
-    complex<double> x; //define complex
+    complex<double> z; //define complex
     double a, b; //define real (2 vars)
-    double y; //define real (1 var)
+    double x; //define real (1 var)
     string ans;
     
     cout << "Available commands (enter function which marked inside parentheses):\nBasic operations:\nadd (a), subtract (s), multiply (m), divide (d), absolute (abs), square root (sqrt)\nLogarithms:\ncommon base10 logarithm (log10), natural logarithm (ln)\nTrigonometry:\nsine (sin), cosine (cos), tangent (tan)\nInverse:\nsine (arcsin), cosine (arccos), tangent (arctan)\nHyperbolic:\nsine (sinh), cosine (cosh), tangent (tanh)\nInverse hyperbolic:\nsine (arcsinh), cosine (arccosh), tangent (arctanh)" << endl;
@@ -27,13 +27,19 @@ int main(){
     }
     
     string onearg[] = {"abs", "sqrt", "log10", "ln", "sin", "cos", "tan", "arcsin", "arccos", "arctan", "sinh", "cosh", "tanh", "arcsinh", "arccosh", "arctanh"};
+    string realarg[] = {"sec", "csc", "cot"}
     int d = sizeof(onearg) / sizeof(*onearg);
+    int f = sizeof(realarg) / sizeof(*realarg);
     
-    bool singlchk = find(onearg, onearg + d, ans) != onearg + d; //checks if the math function need 1 or 2 args
+    bool singlchk = find(onearg, onearg + d, ans) != onearg + d; //checks if the math function needs 1 or 2 args
+    bool realchk = find(realarg, realarg + f, ans) != realarg + f; //checks if specific function needs 1 real arg
     
     if(singlchk){
-        cout << "x = ? (in form of a + bi, enter value as \"a, b\"): ";
-        cin >> x;
+        cout << "z = ? (in form of a + bi, enter value as \"a, b\"): ";
+        cin >> z;
+    }else if(realchk){
+        cout << "x = ? (im form of real numbers, enter value as \"x\"): ";
+        cin << x;
     }else{
         cout << "x = ? (in form of real numbers, enter value as \"x\"): ";
         cin >> a;
@@ -56,53 +62,53 @@ int main(){
         }
         
     }else if(ans == "abs"){ //absolute value
-        cout << real(abs(x)) << to_string((imag(abs(x)) > 0) ? ("+%d", imag(abs(x))) : imag(abs(x))) << "i";
+        cout << real(abs(z)) << to_string((imag(abs(z)) > 0) ? ("+%d", imag(abs(z))) : imag(abs(z))) << "i";
     }else if(ans == "sqrt"){ //square root
-        cout << real(sqrt(x)) << to_string((imag(sqrt(x)) > 0) ? ("+%d", imag(sqrt(x))) : imag(sqrt(x))) << "i";
+        cout << real(sqrt(z)) << to_string((imag(sqrt(z)) > 0) ? ("+%d", imag(sqrt(z))) : imag(sqrt(z))) << "i";
         
     }else if(ans == "log10"){ //common log base 10
-        if (imag(log10(x)) > 0) cout << real(log10(x)) << imag(log10(x)) << "i";
-        else cout << real(log10(x)) << "+" << imag(log10(x)) << "i";
+        if (imag(log10(z)) > 0) cout << real(log10(z)) << imag(log10(z)) << "i";
+        else cout << real(log10(z)) << "+" << imag(log10(z)) << "i";
     }else if(ans == "ln"){ //natural log 
-        if (imag(log(x)) > 0) cout << real(log(x)) << imag(log(x)) << "i";
-        else cout << real(log(x)) << "+" << imag(log(x)) << "i";
+        if (imag(log(z)) > 0) cout << real(log(z)) << imag(log(z)) << "i";
+        else cout << real(log(z)) << "+" << imag(log(z)) << "i";
         
     }else if(ans == "sin"){ //trigo
-        cout << real(sin(x)) << to_string((imag(sin(x)) > 0) ? ("+%d", imag(sin(x))) : imag(sin(x))) << "i";
+        cout << real(sin(z)) << to_string((imag(sin(z)) > 0) ? ("+%d", imag(sin(z))) : imag(sin(z))) << "i";
     }else if(ans == "cos"){
-        cout << real(cos(x)) << to_string((imag(cos(x)) > 0) ? ("+%d", imag(cos(x))) : imag(cos(x))) << "i";
+        cout << real(cos(z)) << to_string((imag(cos(z)) > 0) ? ("+%d", imag(cos(z))) : imag(cos(z))) << "i";
     }else if(ans == "tan"){
-        cout << real(tan(x)) << to_string((imag(tan(x)) > 0) ? ("+%d", imag(tan(x))) : imag(tan(x))) << "i";
+        cout << real(tan(z)) << to_string((imag(tan(z)) > 0) ? ("+%d", imag(tan(z))) : imag(tan(z))) << "i";
     }else if(ans == "cot"){
         cout << 1.0 / tan(y)
     }else if(ans == "sec"){
         cout << 1.0 / cos(y)
     }else if(ans == "csc"){
-        //cout << 
+        cout << 1.0 / sin(y)
         
     }else if(ans == "arcsin"){ //inverse
-        cout << real(asin(x)) << to_string((imag(asin(x)) > 0) ? ("+%d", imag(asin(x))) : imag(asin(x))) << "i";
+        cout << real(asin(z)) << to_string((imag(asin(z)) > 0) ? ("+%d", imag(asin(z))) : imag(asin(z))) << "i";
     }else if(ans == "arccos"){
-        cout << real(acos(x)) << to_string((imag(acos(x)) > 0) ? ("+%d", imag(acos(x))) : imag(acos(x))) << "i";
+        cout << real(acos(z)) << to_string((imag(acos(z)) > 0) ? ("+%d", imag(acos(z))) : imag(acos(z))) << "i";
     }else if(ans == "arctan"){
-        cout << real(atan(x)) << to_string((imag(atan(x)) > 0) ? ("+%d", imag(atan(x))) : imag(atan(x))) << "i";
+        cout << real(atan(z)) << to_string((imag(atan(z)) > 0) ? ("+%d", imag(atan(z))) : imag(atan(z))) << "i";
         
     }else if(ans == "sinh"){ //hyp
-        cout << real(sinh(x)) << to_string((imag(sinh(x)) > 0) ? ("+%d", imag(sinh(x))) : imag(sinh(x))) << "i";
+        cout << real(sinh(z)) << to_string((imag(sinh(z)) > 0) ? ("+%d", imag(sinh(z))) : imag(sinh(z))) << "i";
     }else if(ans == "cosh"){
-        cout << real(cosh(x)) << to_string((imag(cosh(x)) > 0) ? ("+%d", imag(cosh(x))) : imag(cosh(x))) << "i";
+        cout << real(cosh(z)) << to_string((imag(cosh(z)) > 0) ? ("+%d", imag(cosh(z))) : imag(cosh(z))) << "i";
     }else if(ans == "tanh"){
-        cout << real(tanh(x)) << to_string((imag(tanh(x)) > 0) ? ("+%d", imag(tanh(x))) : imag(tanh(x))) << "i";
+        cout << real(tanh(z)) << to_string((imag(tanh(z)) > 0) ? ("+%d", imag(tanh(z))) : imag(tanh(z))) << "i";
         
     }else if(ans == "arcsinh"){ //inverse hyp
-        if (imag(asinh(x)) > 0) cout << real(asinh(x)) << imag(asinh(x)) << "i";
-        else cout << real(asinh(x)) << "+" << imag(asinh(x)) << "i";
+        if (imag(asinh(z)) > 0) cout << real(asinh(z)) << imag(asinh(z)) << "i";
+        else cout << real(asinh(z)) << "+" << imag(asinh(z)) << "i";
     }else if(ans == "arccosh"){
-        if (imag(acosh(x)) > 0) cout << real(acosh(x)) << imag(acosh(x)) << "i";
-        else cout << real(acosh(x)) << "+" << imag(acosh(x)) << "i";
+        if (imag(acosh(z)) > 0) cout << real(acosh(z)) << imag(acosh(z)) << "i";
+        else cout << real(acosh(z)) << "+" << imag(acosh(z)) << "i";
     }else if(ans == "arctanh"){
-        if (imag(atanh(x)) > 0) cout << real(atanh(x)) << imag(atanh(x)) << "i";
-        else cout << real(atanh(x)) << "+" << imag(atanh(x)) << "i";
+        if (imag(atanh(z)) > 0) cout << real(atanh(z)) << imag(atanh(z)) << "i";
+        else cout << real(atanh(z)) << "+" << imag(atanh(z)) << "i";
     }                          
     return 0;
 }
